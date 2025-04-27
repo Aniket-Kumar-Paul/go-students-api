ed89748088099dcc25d229c1f794b63789c1a7ea
Aniket Kumar Paul <aniketkumarpaul1@gmail.com>
2025-04-27 13:39:50 +0530

    Fetching all student records

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,5 @@ import "github.com/Aniket-Kumar-Paul/go-students-api/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error) // (id, error)
 	GetStudentById(id int64) (types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
